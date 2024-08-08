c1e33d6ba6f620c72446efa988cbf6ddf9f11069
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-08-08 14:21:35 -0500

    Add sorting events by next event date from earliest to latest

--- src/main/java/com/lulunac27a/eventcalculator/EventController.java
+++ src/main/java/com/lulunac27a/eventcalculator/EventController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 
 @Controller
 public class EventController {
@@ -28,6 +29,14 @@ public class EventController {
 
     @GetMapping("/event-list") // events list page
     public String getEventsList(Model model) {
+        Comparator<Event> comparator = Comparator
+                .comparing(o -> o.getNextEventDate(o.getStartDate(), o.getRepeatInterval(), o.getRepeatOften()));// sort
+                                                                                                                 // events
+                                                                                                                 // by
+                                                                                                                 // next
+                                                                                                                 // event
+                                                                                                                 // date
+        eventWrapper.getEvents().sort(comparator);// sort events using comparator
         model.addAttribute("eventList", eventWrapper);// get list of all events
         return "events-list";// show list of events
     }
