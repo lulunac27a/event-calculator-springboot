d88d7b39eefc41b8e996b4a9d167d60b6bf682d3
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-08-07 23:12:48 -0500

    Create variable for current date

--- src/main/java/com/lulunac27a/eventcalculator/entity/Event.java
+++ src/main/java/com/lulunac27a/eventcalculator/entity/Event.java
@@ -28,29 +28,30 @@ public class Event {// event class with name, event start date, event repeat int
     }
 
     public LocalDate getNextEventDate(LocalDate eventStartDate, RepeatInterval repeatInterval, int repeatOften) {
+        LocalDate currentDate = LocalDate.now();// current date
         LocalDate nextEventDate = null;// calculate next event date based on event start date, repeat interval and
                                        // repeat often
         switch (repeatInterval) {
             case DAILY:// daily events
-                int dayDifference = (int) ChronoUnit.DAYS.between(eventStartDate, LocalDate.now());// calculate date
-                                                                                                   // difference
+                int dayDifference = (int) ChronoUnit.DAYS.between(eventStartDate, currentDate);// calculate date
+                                                                                               // difference
                 int dailyTimes = (dayDifference + repeatOften - 1) / repeatOften;// calculate number of times event
                                                                                  // happened
                 nextEventDate = eventStartDate.plusDays((long) repeatOften * dailyTimes);// calculate next event date by
                                                                                          // adding date
                 break;
             case WEEKLY:// weekly events
-                int weekDifference = (int) ChronoUnit.WEEKS.between(eventStartDate, LocalDate.now());
+                int weekDifference = (int) ChronoUnit.WEEKS.between(eventStartDate, currentDate);
                 int weeklyTimes = (weekDifference + repeatOften - 1) / repeatOften;
                 nextEventDate = eventStartDate.plusWeeks((long) repeatOften * weeklyTimes);
                 break;
             case MONTHLY:// monthly events
-                int monthDifference = (int) ChronoUnit.MONTHS.between(eventStartDate, LocalDate.now());
+                int monthDifference = (int) ChronoUnit.MONTHS.between(eventStartDate, currentDate);
                 int monthlyTimes = (monthDifference + repeatOften - 1) / repeatOften;
                 nextEventDate = eventStartDate.plusMonths((long) repeatOften * monthlyTimes);
                 break;
             case YEARLY:// yearly events
-                int yearDifference = (int) ChronoUnit.YEARS.between(eventStartDate, LocalDate.now());
+                int yearDifference = (int) ChronoUnit.YEARS.between(eventStartDate, currentDate);
                 int yearlyTimes = (yearDifference + repeatOften - 1) / repeatOften;
                 nextEventDate = eventStartDate.plusYears((long) repeatOften * yearlyTimes);
                 break;
